62357165884796a1da28f85c0786db2c770e06b0
omkaaaaaar <omkarpatkarr@gmail.com>
2026-03-25 23:16:01 +0530

    Update pydantic_type_validation.py

--- pydantic_type_validation.py
+++ pydantic_type_validation.py
@@ -1,14 +1,14 @@
-from pydantic import BaseModel, EmailStr, AnyUrl
-from typing import List, Dict, Optional
+from pydantic import BaseModel, EmailStr, AnyUrl, Field
+from typing import List, Dict, Optional, Annotated
 
 class Patient(BaseModel):
-    name: str
-    age: int
+    name: Annotated[str, Field(max_length=50, title='Name of the patient', description='The name should not exceed 50 characters', examples=['Omkar', 'Karan'])] #here we are using Annotated to provide additional metadata for the name field, max_length is used to validate that the name should not exceed 50 characters, title and description are used to provide additional information about the field
+    age: int = Field(gt=0, lt=120) #here we are using field function to validate that age should be greater than 0 and less than 120
     email: EmailStr
     linkedin_url: Optional[AnyUrl]=None
-    weight: float
+    weight: Annotated[float, Field(gt=0, strict=True)] #here we are using Annotated to provide additional metadata for the weight field, gt is used to validate that weight should be greater than 0, strict is used to ensure that the value provided for weight is of type float
     married: bool = False #here we are providing a default value to married, if user does not provide any value for married, it will be considered as False
-    allergies: Optional[List[str]] = None #list of strings, to validate that every value in the list is a string we used [str], None is the default value here, it means if user does not provide any allergies it will be considered as None
+    allergies: Annotated[Optional[List[str]], Field(default=None, description='A list of allergies that the patient has')] = None #here we are using Annotated to provide additional metadata for the allergies field, default is set to None, which means that if user does not provide any value for allergies, it will be considered as None, description is used to provide additional information about the field
     contact_details: Dict[str, str] #dictionary with string keys and string values
 
 
