18e807320e201ee46d5135a4d913ee5103fccd8f
omkaaaaaar <omkarpatkarr@gmail.com>
2026-03-27 01:14:48 +0530

    Update pydantic_type_validation.py

--- pydantic_type_validation.py
+++ pydantic_type_validation.py
@@ -6,7 +6,7 @@ class Patient(BaseModel):
     age: int = Field(gt=0, lt=120) #here we are using field function to validate that age should be greater than 0 and less than 120
     email: EmailStr
     linkedin_url: Optional[AnyUrl]=None
-    weight: Annotated[float, Field(gt=0, strict=True)] #here we are using Annotated to provide additional metadata for the weight field, gt is used to validate that weight should be greater than 0, strict is used to ensure that the value provided for weight is of type float
+    weight: Annotated[float, Field(gt=0, strict=True)] #here we are using Annotated to provide additional metadata for the weight field, gt is used to validate that weight should be greater than 0, strict tells the pydantic to not do the type coercion(type conversion)
     married: bool = False #here we are providing a default value to married, if user does not provide any value for married, it will be considered as False
     allergies: Annotated[Optional[List[str]], Field(default=None, description='A list of allergies that the patient has')] = None #here we are using Annotated to provide additional metadata for the allergies field, default is set to None, which means that if user does not provide any value for allergies, it will be considered as None, description is used to provide additional information about the field
     contact_details: Dict[str, str] #dictionary with string keys and string values
